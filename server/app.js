61684a007c15246f556c475c40ef629b515e0ef7
Aseem Khullar <khullar26aseem@outlook.com>
2020-04-24 05:24:18 +0530

    Done completely

--- server/app.js
+++ server/app.js
@@ -41,6 +41,14 @@ app.use(require('./routes/user'))
 //Port for listening
 const port = process.env.PORT || 8000;
 
+if(process.env.NODE_ENV=="production"){
+    api.use(express.static('client/build'))
+    const path = require('path')
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
+}
+
 app.listen(port, () => {
     console.log("Server is running on ", port)
 });
